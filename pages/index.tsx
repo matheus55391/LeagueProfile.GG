d04c9c145a549acd5283f569ca640b1764161f7d
matheus55391 <matheus.manulo55@gmail.com>
2022-08-09 08:59:39 -0300

    fix color

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const [selected, setSelected] = useState(1)
@@ -19,21 +18,21 @@ const Home: NextPage = () => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-5 " viewBox="0 0 20 20" fill="currentColor">
             <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
           </svg>
-          <span className={`mx-4 text-${selected === 1 ? 'white' : 'gray-400' }  hover:text-white`}>Início</span>
+          <span className={`mx-4 text-${selected === 1 ? 'red' : 'gray-400' } `}>Início</span>
         </button>
         
         <button className='flex mx-4 my-2 pr-8' onClick={()=>{setSelected(2)}}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
           </svg>
-          <span className={`mx-4 text-${selected === 2 ? 'white' : 'gray-400' }  hover:text-white`}>Estatísticas</span>
+          <span className={`mx-4 text-${selected === 2 ? 'white' : 'gray-400' } `}>Estatísticas</span>
         </button>
 
         <button className='flex mx-4 my-2 pr-8' onClick={()=>{setSelected(3)}}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
           </svg>
-          <span className={`mx-4 text-${selected === 3 ? 'white' : 'gray-400' }  hover:text-white`}>Pro Builds</span>
+          <span className={`mx-4 text-${selected === 3 ? 'white' : 'gray-400' }`}>Pro Builds</span>
         </button>
 
         <button className='flex mx-4 my-2 pr-8 mt-auto' onClick={()=>{setSelected(4)}}>
